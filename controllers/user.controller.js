01dec44c584e974f34b7785ab4869ea5116b8637
Nirmal Kumar <nirmal1064@gmail.com>
2021-08-28 15:00:50 +0530

    using try catch

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,42 +2,38 @@ const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const register = (req, res) => {
+const register = async (req, res) => {
     const { username, password } = req.body;
     if (!username || !password) {
-        res.status(400).json({msg: "Please enter all the fields"});
-    } else if (password.length < 6) {
-        res.status(400).json({msg: "Password should be atleast 6 characters"});
-    } else {
-        User.findOne({username: username.toString().trim()}, (err, user) => {
-            if (err) res.status(500).send({ msg: "Some error occured" });
-            else if(user) res.status(409).json({msg: "Existing User. Please login"});
-            else {
-                const hashedPassword = bcrypt.hashSync(password, 8);
-                const newUser = new User({
-                    username,
-                    password : hashedPassword
-                });
-                newUser.save((err, user) => {
-                    if (err) {
-                        console.log(err);
-                        res.status(500).json({msg: err.message});
-                    } else {
-                        jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
-                            expiresIn: 86400
-                        }, (err, token) => {
-                            if (err) res.status(500).json({msg: "Some Error occured"});
-                            res.status(201).json({
-                                id: user._id,
-                                username: user.username,
-                                auth: true,
-                                token: token
-                            });
-                        });
-                    }
-                });        
-            }
+        return res.status(400).json({msg: "Please enter all the fields"});
+    }
+    if (password.length < 6) {
+        return res.status(400).json({msg: "Password should be atleast 6 characters"});
+    }
+    try {
+        const existingUser = await User.findOne({username: username.toString().trim()});
+        if (existingUser) {
+            return res.status(409).json({msg: "Existing User. Please login"});
+        }
+        const hashedPassword = bcrypt.hashSync(password, 8);
+        const newUser = new User({
+            username,
+            password : hashedPassword
         });
+        const user = await newUser.save();
+        console.log("User created successfully");
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
+            expiresIn: 86400
+        });
+        res.status(201).json({
+            id: user._id,
+            username: user.username,
+            auth: true,
+            token: token
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({msg: err.message});
     }
 }
 
--- models/user.model.js
+++ models/user.model.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
+        lowercase: true,
         minlength: 5
     },
     password: {
