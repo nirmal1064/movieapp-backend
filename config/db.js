d479e104f05ee8f40ef5256c5d86e85ae869cf85
Nirmal Kumar <nirmal1064@gmail.com>
2021-09-03 02:28:06 +0530

    added eslint

--- config/db.js
+++ config/db.js
@@ -2,14 +2,17 @@ const mongoose = require("mongoose");
 const MONGO_URI = process.env.MONGO_URI;
 
 exports.connect = () => {
-    mongoose.connect(MONGO_URI, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        retryWrites: true
-    }).then(() => {
-        console.log("Connected to MongoDB Successfully");
-    }).catch(err => {
-        console.error("Connection error", err);
-        process.exit();
+  mongoose
+    .connect(MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      retryWrites: true
+    })
+    .then(() => {
+      console.log("Connected to MongoDB Successfully");
+    })
+    .catch((err) => {
+      console.error("Connection error", err);
+      process.exit();
     });
-}
+};
