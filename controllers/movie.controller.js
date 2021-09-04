21bbf20cd3460a60d6b585a18a867011e083d43e
Nirmal Kumar <nirmal1064@gmail.com>
2021-09-04 22:25:04 +0530

    work in progress

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -3,24 +3,6 @@ const Movie = require("../models/movie.model");
 const Watched = require("../models/watch.model").watched;
 const WatchList = require("../models/watch.model").watchList;
 
-const addMovieToDb = async (req, res) => {
-  const _id = req.body.imdbID;
-  try {
-    const newMovie = new Movie({
-      _id: req.body.imdbID,
-      ...req.body
-    });
-    const movie = await Movie.findByIdAndUpdate(_id, newMovie, {
-      new: true,
-      upsert: true
-    });
-    res.status(201).json(movie);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ msg: error.message });
-  }
-};
-
 const addMovie = async (movieId) => {
   const url = `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&i=${movieId}`;
   const data = await (await axios.get(url)).data;
@@ -81,7 +63,7 @@ const addMovieToWatchList = async (req, res) => {
         res.status(200).json({ msg: "Movie already in collection" });
       }
     } else {
-      // create a watched user and add movie to the collection
+      // create a watchlist user and add movie to the collection
       const newWatchedDoc = await WatchList.create({
         userId,
         movies: [movieId]
@@ -94,4 +76,79 @@ const addMovieToWatchList = async (req, res) => {
   }
 };
 
-module.exports = { addMovieToDb, addMovieToWatched, addMovieToWatchList };
+const removeMovieFromWatched = async (req, res) => {
+  const { userId, movieId } = req.body;
+  try {
+    const user = await Watched.findOne({ userId });
+    const movieIndex = user.movies.findIndex(movieId);
+    if (movieIndex > -1) {
+      user.movies.splice(movieIndex, 1);
+    }
+    const updateWatch = await user.save();
+    res.status(201).json(updateWatch);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+const removeMovieFromWatchList = async (req, res) => {
+  const { userId, movieId } = req.body;
+  try {
+    const user = await WatchList.findOne({ userId });
+    const movieIndex = user.movies.findIndex(movieId);
+    if (movieIndex > -1) {
+      user.movies.splice(movieIndex, 1);
+    }
+    const updateWatch = await user.save();
+    res.status(201).json(updateWatch);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+const getWatchedMovies = async (req, res) => {
+  const { userId } = req.body;
+  try {
+    const user = await Watched.findOne({ userId });
+    if (user) {
+      const watchedMovies = await Watched.findOne({ userId }).populate(
+        "movies"
+      );
+      res.status(200).json(watchedMovies);
+    } else {
+      res.status(200).json({ msg: "Something wrong" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(error.status).json({ msg: error.message });
+  }
+};
+
+const getWatchListMovies = async (req, res) => {
+  const { userId } = req.body;
+  try {
+    const user = await WatchList.findOne({ userId });
+    if (user) {
+      const watchedMovies = await WatchList.findOne({ userId }).populate(
+        "movies"
+      );
+      res.status(200).json(watchedMovies);
+    } else {
+      res.status(200).json({ msg: "Something wrong" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(error.status).json({ msg: error.message });
+  }
+};
+
+module.exports = {
+  addMovieToWatched,
+  addMovieToWatchList,
+  removeMovieFromWatched,
+  removeMovieFromWatchList,
+  getWatchedMovies,
+  getWatchListMovies
+};
--- routes/movie.router.js
+++ routes/movie.router.js
@@ -1,13 +1,20 @@
 const {
-  addMovieToDb,
   addMovieToWatched,
-  addMovieToWatchList
+  addMovieToWatchList,
+  getWatchedMovies,
+  getWatchListMovies,
+  removeMovieFromWatched,
+  removeMovieFromWatchList
 } = require("../controllers/movie.controller");
+const { verifyToken } = require("../middlewares/auth");
 
 const router = require("express").Router();
 
-router.post("/add", addMovieToDb);
-router.post("/watched/add", addMovieToWatched);
-router.post("/watchlist/add", addMovieToWatchList);
+router.post("/watched/add", verifyToken, addMovieToWatched);
+router.post("/watchlist/add", verifyToken, addMovieToWatchList);
+router.post("/watched/remove", verifyToken, removeMovieFromWatched);
+router.post("/watchlist/remove", verifyToken, removeMovieFromWatchList);
+router.get("/watched", verifyToken, getWatchedMovies);
+router.get("/watchlist", verifyToken, getWatchListMovies);
 
 module.exports = router;
