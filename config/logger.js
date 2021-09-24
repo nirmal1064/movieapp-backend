55126f4d70edb1c7a706d2177e17350beecb9cc4
Nirmal Kumar <nirmal1064@gmail.com>
2021-09-24 20:12:38 +0530

    Heroku

--- config/logger.js
+++ config/logger.js
@@ -10,14 +10,14 @@ const logger = createLogger({
     format.splat(),
     format.json()
   ),
-  defaultMeta: { service: "movie-service" },
-  transports: [
-    new transports.File({ filename: "movie.error.log", level: "error" }),
-    new transports.File({ filename: "movie.combined.log" })
-  ]
+  defaultMeta: { service: "movie-service" }
 });
 
 if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new transports.File({ filename: "movie.error.log", level: "error" })
+  );
+  logger.add(new transports.File({ filename: "movie.combined.log" }));
   logger.add(
     new transports.Console({
       format: format.combine(format.colorize(), format.simple())
